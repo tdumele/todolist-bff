7c7ba0fcedb194c9c9d1e275d10206ecdca76d77
Teddy Dumélé <teddy.dumele@gmail.com>
2024-11-17 14:02:51 +0100

    :tada: init project

---
+++ src/main/java/com/todolist/bff_todolist/BffTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.bff_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffTodolistApplication.class, args);
+	}
+
+}
