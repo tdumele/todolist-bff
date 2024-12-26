7f0be1050149233b44c7badbe7bbbda85804c580
Teddy Dumélé <teddy.dumele@gmail.com>
2024-12-26 16:06:01 +0100

    :wrench: fix CORS problems with preflight

--- src/main/java/com/todolist/bff_todolist/security/SecurityConfiguration.java
+++ src/main/java/com/todolist/bff_todolist/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.todolist.bff_todolist.config.ApplicationProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -11,15 +12,21 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.security.SecureRandom;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,14 +40,15 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers(applicationProperties.getSecurity().getPermittedUrls()).permitAll();
-                    authorize.anyRequest().authenticated();
+                    authorize.requestMatchers(applicationProperties.getSecurity().getPermittedUrls()).permitAll()
+                            .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                            .anyRequest().authenticated();
                 })
                 .authenticationProvider(authenticationProvider(userDetailsService))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        http.headers().frameOptions().disable();
+
         return http.build();
     }
 
@@ -50,7 +58,8 @@ public class SecurityConfiguration {
 
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(applicationProperties.getSecurity().getAllowedOrigins());
+                registry.addMapping("/**")
+                        .allowedOrigins(applicationProperties.getSecurity().getAllowedOrigins());
             }
         };
     }
